3cd028da0c6754d090ef11c2fe2fc6fc6abfa157
JiXu <13329239941@163.com>
2025-05-04 14:41:38 +0800

    feat(beans): 添加 Bean 的初始化和销毁方法支持
    添加 InitializingBean 接口，用于在 Bean 初始化后执行自定义逻辑。实现该接口的类需要提供 afterPropertiesSet 方法，以完成额外的初始化操作。

---
+++ src/main/java/org/qlspringframework/beans/factory/InitializingBean.java
@@ -0,0 +1,12 @@
+package org.qlspringframework.beans.factory;
+
+/**
+ * @author jixu
+ * @title InitializingBean
+ * @date 2025/5/4 00:18
+ */
+public interface InitializingBean {
+
+
+    void afterPropertiesSet();
+}
